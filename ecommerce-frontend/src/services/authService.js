3fb5fb84694ddd81a370c9a74ffd874f029c21c3
Abhishek Kumar <157559756+32342Abhishek@users.noreply.github.com>
2025-09-13 07:49:09 +0530

    Update API_URL to production endpoint

--- ecommerce-frontend/src/services/authService.js
+++ ecommerce-frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:9090/back1/auth";
+const API_URL = "ec2-51-21-199-152.eu-north-1.compute.amazonaws.com";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
